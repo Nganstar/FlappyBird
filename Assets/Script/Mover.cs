7a739e07498d0388f80cafd6f47230684e59fb22
Nganstar <ngan554@gmail.com>
2017-10-22 20:30:32 -0400

    changes

---
+++ Assets/Script/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    public float speed = 1.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveObstacle();
+		
+	}
+    void MoveObstacle()
+    {
+        transform.Translate(-speed*Time.deltaTime, 0, 0);
+    }
+}
