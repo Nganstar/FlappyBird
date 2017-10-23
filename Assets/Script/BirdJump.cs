9833da71fcc8799c6b39016c9981e874903ae6d6
Nganstar <ngan554@gmail.com>
2017-10-23 00:27:10 -0400

    almost done

---
+++ Assets/Script/BirdJump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class BirdJump : MonoBehaviour {
+    Rigidbody2D bird;
+    public static int score = 0;
+    public Text scoreText;
+	// Use this for initialization
+	void Start () {
+        bird = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            bird.velocity = new Vector2(0, 5);
+        }
+        scoreText.text = score.ToString();
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "point")
+        {
+            score++;
+            return;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Pipes" || collision.gameObject.tag=="Bars")
+        {
+            score = 0;
+            Destroy(gameObject);
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Script/MenuCOnt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuCOnt : MonoBehaviour {
+    public GameObject playButton;
+    public GameObject muteButton;
+    public GameObject menu;
+    public GameObject Ok;
+    public GameObject cameras;
+    public Button SoundButton;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GetOption()
+    {
+        playButton.SetActive(false);
+        muteButton.SetActive(true);
+        menu.SetActive(false);
+        Ok.SetActive(true);
+    }
+    public void okay()
+    {
+        playButton.SetActive(true);
+        menu.SetActive(true);
+        Ok.SetActive(false);
+        muteButton.SetActive(false);
+    }
+    public void Mute()
+    {
+
+    }
+}
